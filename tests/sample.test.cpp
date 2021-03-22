37af23b91a9324dbb11fd714ff9e50a7c1bbb1a0
Matteo Bonacini <matteo.bonna@gmail.com>
2021-03-22 20:44:31 +0100

    Add AI randInt test

--- tests/sample.test.cpp
+++ tests/sample.test.cpp
@@ -1,10 +1,8 @@
+// Doctest requires this define to be specified only once.
+// Probably this is the best place
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../doctest.h"
 
 TEST_CASE("Sample test") {
   CHECK(true);
-}
-
-TEST_CASE("Sample test 2") {
-  CHECK(false);
 }
