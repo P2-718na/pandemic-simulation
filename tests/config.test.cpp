fb26aa8246baa737ca01e4a5b171294b97f70024
Matteo Bonacini <matteo.bonna@gmail.com>
2021-05-26 12:11:31 +0200

    Add chanceCheck tests

--- tests/config.test.cpp
+++ tests/config.test.cpp
@@ -62,3 +62,61 @@ TEST_CASE("Check Config::randInt function") {
     CHECK(good);
   }
 }
+
+TEST_CASE("Check Config::chanceCheck function") {
+  const int iterations = 100000;
+  float success = 0.f;
+  float fail = 0.f;
+
+  SUBCASE("Chance greater than 1") {
+    bool good = true;
+
+    for (int i = 0; i < iterations; ++i) {
+      if (!Config::chanceCheck(1.1)) {
+        good = false;
+        break;
+      }
+    }
+
+    CHECK(good);
+  }
+
+  SUBCASE("Chance is 5%") {
+    for (int i = 0; i < iterations; ++i) {
+      if (Config::chanceCheck(.5)) {
+        success++;
+        continue;
+      }
+
+      fail++;
+    }
+
+    CHECK_EQ(success / fail, doctest::Approx(1).epsilon(0.01));
+  }
+
+  SUBCASE("Chance is 20%") {
+    for (int i = 0; i < iterations; ++i) {
+      if (Config::chanceCheck(.2)) {
+        success++;
+        continue;
+      }
+
+      fail++;
+    }
+
+    CHECK_EQ(success / fail, doctest::Approx(0.25).epsilon(0.01));
+  }
+
+  SUBCASE("Chance is 75%") {
+    for (int i = 0; i < iterations; ++i) {
+      if (Config::chanceCheck(.75)) {
+        success++;
+        continue;
+      }
+
+      fail++;
+    }
+
+    CHECK_EQ(success / fail, doctest::Approx(3).epsilon(0.01));
+  }
+}
