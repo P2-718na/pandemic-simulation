0535139b32656106856673ce98905e93690f1ed5
Matteo Bonacini <matteo.bonna@gmail.com>
2021-06-09 18:07:07 +0200

    Format tests

--- tests/config.test.cpp
+++ tests/config.test.cpp
@@ -1,5 +1,5 @@
-#include "doctest.h"
 #include "config.hpp"
+#include "doctest.h"
 
 using pandemic::Config;
 
--- tests/pathfinder.test.cpp
+++ tests/pathfinder.test.cpp
@@ -1,6 +1,6 @@
+#include "pathfinder.hpp"
 #include <iostream>
 #include "doctest.h"
-#include "pathfinder.hpp"
 #include "types.hpp"
 
 using pandemic::Pathfinder;
@@ -8,20 +8,20 @@ using pandemic::Pathfinder;
 TEST_CASE("Test pathfinder") {
   Pathfinder pathfinder;
 
-  pathfinder.loadPath({0, 0}, {3, 5});
+  pathfinder.loadPath({ 0, 0 }, { 3, 5 });
 
   CHECK_EQ(pathfinder.getPath().size(), 5);
 
   auto step = pathfinder.step();
-  CHECK_EQ(step, pandemic::Coords{1, 1});
+  CHECK_EQ(step, pandemic::Coords{ 1, 1 });
   step = pathfinder.step();
-  CHECK_EQ(step, pandemic::Coords{2, 2});
+  CHECK_EQ(step, pandemic::Coords{ 2, 2 });
   step = pathfinder.step();
-  CHECK_EQ(step, pandemic::Coords{3, 3});
+  CHECK_EQ(step, pandemic::Coords{ 3, 3 });
   step = pathfinder.step();
-  CHECK_EQ(step, pandemic::Coords{3, 4});
+  CHECK_EQ(step, pandemic::Coords{ 3, 4 });
   step = pathfinder.step();
-  CHECK_EQ(step, pandemic::Coords{3, 5});
+  CHECK_EQ(step, pandemic::Coords{ 3, 5 });
   step = pathfinder.step();
-  CHECK_EQ(step, pandemic::Coords{3, 5});
+  CHECK_EQ(step, pandemic::Coords{ 3, 5 });
 }
